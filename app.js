9658dd4d2aa5ca5851a72372c1a08af9a39f74ba
rionk <rionwolfz@daum.net>
2024-10-11 21:38:44 +0900

    End of day progress for contact form feature

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var cookieParser = require("cookie-parser")
 const router = require("./router")
 const app = express()
 
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 app.use(express.static("public"))
 app.use(cookieParser())
